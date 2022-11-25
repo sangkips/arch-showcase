88c4fe65b4b4b56cce923ac9a93a6332f7158831
sangkips <sangkipkoech@gmail.com>
2022-11-25 16:38:09 +0300

    Implemented CustomUserManager

--- showcase/models.py
+++ showcase/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+class CustomUsermanager(BaseUserManager):
+
+    def createUser(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Please provide an email address")
+            email = self.normalize_email(email)
+            user = self.model(email=email, **extra_fields)
+            user.set_password(password)
+            user.save()
+            return user
+
+    
+    def createSuperuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get('is_staff' is not True):
+            raise ValueError('Superuser must have is_staff to be True')
+        if extra_fields.get('is_superuser' is not True):
+            raise ValueError('Superuser must have is_superuser to be True')
+        return self.createUser(email, password, **extra_fields)
+
